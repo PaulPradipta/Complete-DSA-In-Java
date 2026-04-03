f6406952db4f24bddb5556a0e5bcdb8f4307391b
Pradipta Paul <pradipta2003paul@gmail.com>
2026-04-03 19:18:00 +0530

    Implemented Single Inheritance in OOPs in Java

---
+++ Object Oriented Programming/Lecture 5/Single_Level_Inheritance.java
@@ -0,0 +1,21 @@
+class Animal {
+    protected int numberOflegs;
+
+    void eat() {
+        System.out.println("Animals eat food");
+    }
+}
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dogs bark");
+    }
+}
+public class Single_Level_Inheritance {
+    public static void main(String args[]) {
+        Dog obj = new Dog();
+        obj.eat();
+        obj.bark();
+        obj.numberOflegs = 4;
+        System.out.println("Number of Legs a Dog has : " + obj.numberOflegs);
+    }
+}
