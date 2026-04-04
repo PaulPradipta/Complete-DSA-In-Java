32888c45156b6b4e690bc8c68a2c71e21407a10a
Pradipta Paul <pradipta2003paul@gmail.com>
2026-04-04 09:02:31 +0530

    Implemented Finally Block in Java

---
+++ Object Oriented Programming/Lecture 9/Finally Block/Finally_Block.java
@@ -0,0 +1,15 @@
+public class Finally_Block {
+    public static void main(String[] args) {
+        int num =0;
+        try {
+            num = 10 / 0;
+        } catch (Exception e) {
+            System.out.println("Caught an exception: " + e);
+            return;
+        }finally {
+            System.out.println("Program ends here");
+        }
+        System.out.println("result is: " + num);
+
+    }
+}
